55c2de8bee8866ec59feeb0598248c501f09283a
kpp6454 <kpp6454@gmail.com>
2024-04-06 04:51:17 +0530

    todo app done  commit

--- src/components/TodoApp/Todo.jsx
+++ src/components/TodoApp/Todo.jsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export const Todo = () => {
-
   const [todo, setTodo] = useState("");
-  const [todoItems, settodoItems] = useState([])
+  const [todoItems, setTodoItems] = useState([]);
 
-  function submitData(e){
-      e.preventDefault();      
-      settodoItems([todo, ...todoItems]);
-      setTodo("");      
+  function submitData(e) {
+    e.preventDefault();
+    if (todo.trim() !== "") {        
+      setTodoItems([{ text: todo, read: false }, ...todoItems]);
+    }
+    setTodo("");      
   }
 
-  function removeTodo(e){
-    e.target.parentElement.remove() 
+  function removeTodo(index) {
+    const newTodoItems = [...todoItems];
+    newTodoItems.splice(index, 1);
+    setTodoItems(newTodoItems);
   }
+
+  function markAsRead(index) {
+    setTodoItems(todoItems.map((item, i) => {
+      if (i === index) {
+        return { ...item, read: !item.read };
+      }
+      return item;
+    }));
+  }
+
+  const sortedTodoItems = todoItems.sort((a, b) => {
+    if (a.read !== b.read) {
+      // If one todo is marked and the other is not, marked todo goes below
+      return a.read ? 1 : -1;
+    }
+    // Otherwise, maintain original order
+    return 0;
+  });
 
   return (    
     <>
@@ -21,10 +42,14 @@ export const Todo = () => {
           <input onChange={(e) => setTodo(e.target.value)} placeholder='Type here to add a text below' type="text" value={todo} />
           <button type='submit'>Submit</button>
       </form>      
-      <ul className="display-todoitems">
-         {todoItems.map((items) => (
-            <li key={{items}}>{items} <span onClick={(e) => removeTodo(e)} className='del-todo'>&times;</span></li>
-         ))}
+      <ul className="display-todoitems">          
+         {sortedTodoItems.map((item, index) => (          
+            <li key={index} className={item.read ? 'read' : ''}>
+              {item.text} 
+              <span onClick={() => removeTodo(index)} className='del-todo'>&times;</span>
+              <span onClick={() => markAsRead(index)} className='mark'>{item.read ? 'Unmark' : 'Mark'}</span>
+            </li>          
+         ))}                  
       </ul>
     </>
   )
